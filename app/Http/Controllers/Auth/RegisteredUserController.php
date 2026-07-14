11475101d0dc5d27a43a2c78ae7886f05537baa3
tonmoystark <tonmoy.a009@gmail.com>
2026-07-14 18:18:46 +0600

    hash, validation and others

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisteredUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
@@ -11,5 +14,15 @@ class RegisteredUserController extends Controller
     public function create()
     {
         return view('auth.register');
+    }
+    public function store(RegisteredUserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        dd($user);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,62 @@
-<h1>Register page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+
+    <h1>Register</h1>
+
+    <form action="{{ route('register.store') }}" method="POST">
+        @csrf
+
+        <div>
+            <label>Name</label>
+            <input
+                type="text"
+                name="name"
+                placeholder="Enter your name">
+        </div>
+
+        <br>
+
+        <div>
+            <label>Email</label>
+            <input
+                type="email"
+                name="email"
+                placeholder="Enter your email">
+        </div>
+
+        <br>
+
+        <div>
+            <label>Password</label>
+            <input
+                type="password"
+                name="password"
+                placeholder="Password">
+        </div>
+
+        <br>
+
+        <div>
+            <label>Confirm Password</label>
+            <input
+                type="password"
+                name="password_confirmation"
+                placeholder="Confirm password">
+        </div>
+
+        <br>
+
+        <button type="submit">
+            Register
+        </button>
+
+    </form>
+
+</body>
+</html>
