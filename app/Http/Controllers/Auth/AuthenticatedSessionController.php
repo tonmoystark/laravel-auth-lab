7aa00bdb8d53a61f7dac778fefe8ea494a7c95b9
tonmoystark <tonmoy.a009@gmail.com>
2026-07-15 21:02:05 +0600

    completed custom authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -34,9 +34,7 @@ class AuthenticatedSessionController extends Controller
         Auth::logout();
 
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
         return redirect()->route('login');
     }
 }
